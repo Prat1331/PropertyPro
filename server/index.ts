719afee1216784d22e9b54cfc5a259391f227785
skillbasket <shivanisharma232005@gmail.com>
2025-07-03 23:41:01 +0530

    Fix CORS for Railway

--- server/index.ts
+++ server/index.ts
@@ -16,6 +16,7 @@ app.use(cors({
       "http://localhost:3000",
       "http://localhost:5051",
       "https://propertiespro.netlify.app",
+      "https://propertypro-production.up.railway.app",
     ];
     const netlifyPreviewRegex = /^https:\/\/[\w-]+--propertiespro\.netlify\.app$/;
 
